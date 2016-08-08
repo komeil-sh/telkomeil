f4b8b6cb52a175c1243329d8de0d27b89de391e6
komeil-sh <komeil.sh1379@gmail.com>
2016-08-08 18:24:39 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {miladlotfi},
+    sudo_users = {signal100},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
